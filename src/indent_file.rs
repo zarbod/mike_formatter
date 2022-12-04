7be536751c2805b28cba98827daa345acd372644
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2022-12-04 14:59:12 -0600

    Update indent_file.rs

--- src/indent_file.rs
+++ src/indent_file.rs
@@ -8,11 +8,25 @@ fn indent_file(chars: &mut Vec<char>) {
         if i == lines.len() { break; }
         for j in 0.. {
             if j == lines[i].len() { break; }
+
+
             if lines[i][j] == '{' {
                 level += 1;
             } else if lines[i][j] == '}' {
                 level -= 1;
             }
+        }
+    }
+}
+
+fn check_level(level: u8, line: &mut Vec<char>) {
+    let mut k = 0;
+    for j in 0..level {
+        for i in 0..4 {
+            if line[k] != ' ' {
+                line.insert(k, ' ');
+            }
+            k += 1;
         }
     }
 }
